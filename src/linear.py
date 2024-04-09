d8bef22caedef4baa14d6bb6b0747ce4d10f021e
LTF-coding <89148240+LTF-coding@users.noreply.github.com>
2024-04-09 21:45:47 +0800

    Update linear.py

--- src/linear.py
+++ src/linear.py
@@ -2,33 +2,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-# class LinearNet(nn.Module):
-#     def __init__(self, input_size, output_size):
-#         super(LinearNet, self).__init__()
-#         self.fc1 = nn.Linear(input_size, 24)
-#         self.fc2 = nn.Linear(24, 48)
-#         self.fc3 = nn.Linear(48, 36)
-#         self.fc4 = nn.Linear(36, 12)
-#         # self.fc5 = nn.Linear(32, 12)
-#         self.fc5 = nn.Linear(12, output_size)
-#         self.dropout = nn.Dropout(0.5)
-#         self.relu = nn.ReLU()
-#         self.softmax = nn.Softmax(dim=1)
-
-#     def forward(self, x):
-#         x = self.fc1(x)
-#         x = self.relu(x)
-#         x = self.fc2(x)
-#         x = self.relu(x)
-#         x = self.fc3(x)
-#         x = self.relu(x)
-#         x = self.dropout(x)
-#         x = self.fc4(x)
-#         x = self.relu(x)
-#         x = self.fc5(x)
-#         return x
-    
-
 class LinearNet(nn.Module):
     def __init__(self, input_size, output_size):
         super(LinearNet, self).__init__()
@@ -37,9 +10,6 @@ class LinearNet(nn.Module):
             nn.ReLU(),
             nn.Linear(24, 48),
             nn.ReLU(),
-            # nn.Linear(48, 48),
-            # nn.ReLU(),
-            # nn.Dropout(0.5),
             nn.Linear(48, 36),
             nn.ReLU(),
             nn.Dropout(0.5),
@@ -51,35 +21,3 @@ class LinearNet(nn.Module):
     def forward(self, x):
         x = self.layers(x)
         return x
-
-
-
-# class LinearNet(nn.Module):
-#     def __init__(self, input_size, output_size):
-#         super(LinearNet, self).__init__()
-#         self.fc1 = nn.Linear(input_size, 24)
-#         self.fc2 = nn.Linear(24, 32)
-#         self.fc3 = nn.Linear(32, 48)
-#         self.fc4 = nn.Linear(48, 32)
-#         self.fc5 = nn.Linear(32, 12)
-#         self.fc6 = nn.Linear(12, output_size)
-#         self.dropout = nn.Dropout(0.5)
-#         self.relu = nn.ReLU()
-#         self.softmax = nn.Softmax(dim=1)
-
-#     def forward(self, x):
-#         x = self.fc1(x)
-#         x = self.relu(x)
-#         x = self.fc2(x)
-#         x = self.relu(x)
-#         x = self.fc3(x)
-#         x = self.relu(x)
-#         x = self.dropout(x)
-#         x = self.fc4(x)
-#         x = self.relu(x)
-#         x = self.dropout(x)
-#         x = self.fc5(x)
-#         x = self.relu(x)
-#         x = self.fc6(x)
-#         # x = self.softmax(x)
-#         return x
